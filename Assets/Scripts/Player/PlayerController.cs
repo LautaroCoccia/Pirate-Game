25992394b2945aa536432b0554a51652467d40f7
Lautaro Coccia <lautaroauditore@gmail.com>
2022-07-16 03:38:01 -0300

    Fix Digmechanic

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,6 +5,8 @@ using System;
 
 public class PlayerController : MonoBehaviour
 {
+    //enum PlayerStates { Idle, Moving, Digging };
+    //PlayerStates currentState;
     private Rigidbody rb;
     [Space(10f)]
     [Header("-- Movement --")]
@@ -16,8 +18,15 @@ public class PlayerController : MonoBehaviour
 
     [Space(10f)]
     [Header("-- Dig --")]
-    [SerializeField] private float digColddown;
+    [SerializeField] private float digSpeed = 1;
+
+    [SerializeField] private float digColdown;
     [SerializeField] private float digCountdown;
+    [SerializeField] private float digDuration;
+    [SerializeField] private float digCurrentTime;
+    [SerializeField] private bool canDig = false;
+    [SerializeField] private bool isDigging = false;
+
     [Space(10f)]
     [Header("-- Animator --")]
     [SerializeField] private Animator anim;
@@ -25,12 +34,15 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        //currentState = PlayerStates.Idle;
     }
 
     // Update is called once per frame
     void Update()
     {
         Movement();
+        CancelDig();
+        DigMechanic();
     }
     private void FixedUpdate()
     {
@@ -39,7 +51,7 @@ public class PlayerController : MonoBehaviour
     private void Movement()
     {
         // movement logic
-        transform.position = new Vector3(transform.position.x, 0.5f, transform.position.z);
+        //transform.position = new Vector3(transform.position.x, 0.5f, transform.position.z);
 
         hor = Input.GetAxis("Horizontal");
         ver = Input.GetAxis("Vertical");
@@ -53,5 +65,52 @@ public class PlayerController : MonoBehaviour
             Quaternion rotation = Quaternion.LookRotation(movementDirection, Vector3.up);
             rb.rotation = Quaternion.RotateTowards(transform.rotation, rotation, rotationSpeed);
         }
+    }
+
+    private void DigMechanic()
+    {   
+        if(digCountdown >0 && !isDigging)
+        {
+            digCountdown -= Time.deltaTime;
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(digCountdown <= 0 && canDig)
+            {
+                isDigging = true;
+                digCountdown = digColdown;
+                digCurrentTime = digDuration;
+            }
+        }
+        if(isDigging)
+        {
+            digCurrentTime -= Time.deltaTime * digSpeed;
+            if (digCurrentTime <= 0)
+            {
+                isDigging = false;
+                Debug.Log("Finish Dig");
+            }
+        }
+    }
+
+    private void CancelDig()
+    {
+        if(isDigging && movementDirection != Vector3.zero)
+        {
+            isDigging = false;
+            digCountdown = 0;
+            digCurrentTime = digDuration;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 6)
+        {
+            canDig = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        canDig = false;
     }
 }
